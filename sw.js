34fdee0ca3a59f1ce5e044a8895804c935f75cc1
noashamz <anwar@risda.gov.my>
2026-04-03 02:32:30 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,4 @@
+// sw.js - Versi Kosong untuk Atasi Skrin Putih
 self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        fetch(event.request).catch(function() {
-            return caches.match(event.request);
-        })
-    );
+    // Biarkan request terus ke internet tanpa cache buat masa ini
 });
